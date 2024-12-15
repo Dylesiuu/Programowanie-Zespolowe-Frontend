e4095815d306ca4841104dd095e82344585f4440
ruteczeq <rutkowska680@gmail.com>
2024-12-15 23:43:30 +0100

    obslga backendu

--- src/swiping/swipe.js
+++
@@ -1,9 +0,0 @@
-// src/app/swipe.js
-export function fetchAnimals() {
-    return fetch('https://api.example.com/animals')
-      .then(response => response.json())
-      .catch(error => {
-        console.error('Error fetching animals:', error);
-        return [];
-      });
-  }
