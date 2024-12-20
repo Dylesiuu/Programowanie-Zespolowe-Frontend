ffa2127127e603fa0e026b5b610c361e040be4b7
uwroblewska <wroblewska.ula@wp.pl>
2024-12-20 01:37:57 +0100

    Register, login testowe

---
+++ pages/registerPage.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import RegisterForm from "../src/register/components/RegisterForm";
+import styles from "../src/styles/registerPage.module.css";
+
+const RegisterPage = () => {
+  useEffect(() => {
+    document.body.style.margin = "0";
+
+    return () => {
+      document.body.style.margin = "";
+    };
+  }, []);
+  return (
+    <div className={styles.registerPageContainer}>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
---
+++ src/register/components/RegisterForm.jsx
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import styles from "../Register.module.css";
+import { useNavigate } from "react-router-dom";
+
+const RegisterForm = () => {
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const passwordRequirements =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{12,}$/;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!passwordRequirements.test(password)) {
+      setMessage(
+        "Hasło musi mieć co najmniej 12 znaków, zawierać co najmniej jedną dużą literę, jedną małą literę, jedną cyfrę i jeden znak specjalny."
+      );
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setMessage("Hasła muszą być takie same!");
+      return;
+    }
+
+    const userData = { name, lastname, email, password };
+
+    try {
+      const response = await fetch("http://localhost:3000/api/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userData),
+      });
+
+      if (response.ok) {
+        navigate("/SwipePage"); //sprawdzic czy jest ok
+      } else {
+        const errorData = await response.json();
+        setMessage(
+          `Błąd: ${errorData.message || "Wystąpił problem podczas rejestracji"}`
+        );
+      }
+    } catch {
+      setMessage("Wystąpił problem z połączeniem. Spróbuj ponownie później.");
+    }
+  };
+
+  return (
+    <div className={styles.registerFormContainer}>
+      <div className={styles.headerContainer}>
+        <h1 className={styles.title}>PETFINITY</h1>
+      </div>
+      <form onSubmit={handleSubmit} className={styles.registerform}>
+        <h1 className={styles.headerRegister}>Zarejestruj się!</h1>
+        <div className={styles.nameAndLastnameInput}>
+          <input
+            className={styles.userNamesInput}
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            placeholder="Imię"
+          />
+          <input
+            className={styles.userNamesInput}
+            type="text"
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+            required
+            placeholder="Nazwisko"
+          />
+        </div>
+        <div className={styles.userInfoInput}>
+          <input
+            className={styles.userInput}
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            placeholder="Email"
+            autoComplete="off"
+          />
+        </div>
+        <div className={styles.userInfoInput}>
+          <input
+            className={styles.userInput}
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            placeholder="Hasło"
+            autoComplete="new-password"
+          />
+        </div>
+        <div className={styles.userInfoInput}>
+          <input
+            className={styles.userInput}
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+            placeholder="Powtórz hasło"
+            autoComplete="new-password"
+          />
+        </div>
+        <button className={styles.registerButton} type="submit">
+          Zarejestruj
+        </button>
+        {message && <p>{message}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default RegisterForm;
