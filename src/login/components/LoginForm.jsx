522077393e76529842df5441fcf9fe69aea28029
uwroblewska <wroblewska.ula@wp.pl>
2024-12-20 01:57:35 +0100

    zmiana requestu wysylanego do backendu

--- src/login/components/LoginForm.jsx
+++ src/login/components/LoginForm.jsx
@@ -18,7 +18,7 @@ const RegisterForm = () => {
     const userData = { email, password };
 
     try {
-      const response = await fetch("http://localhost:3000/api/login", {
+      const response = await fetch("http://localhost:3000/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(userData),
