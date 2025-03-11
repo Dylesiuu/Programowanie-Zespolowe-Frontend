8d54192d0d2c7719e110750ba9890dc57fbf0a66
uwroblewska <wroblewska.ula@wp.pl>
2025-03-11 18:56:43 +0100

    Add tests to LoginForm and RegisterForm. Small fixes

--- src/login/components/LoginForm.jsx
+++ src/login/components/LoginForm.jsx
@@ -1,78 +1,88 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "../../register/Register.module.css";
-import router from "next/router";
+import { useRouter } from "next/router";
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const LoginForm = () => {
-  //const navigate = useNavigate();
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
+  const [errors, setErrors] = useState({});
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const newErrors = {};
 
-    const userData = { email, password };
+    if (!email) newErrors.email = "Email jest wymagany.";
+    else if (!/\S+@\S+\.\S+/.test(email)) newErrors.email = "Nieprawidłowy format adresu e-mail.";
+    if (!password) newErrors.password = "Hasło jest wymagane.";
+
+    setErrors(newErrors);
+    if(Object.keys(newErrors).length > 0) return;
 
     try {
-      const response = await fetch("http://localhost:3000/auth/login", {
+      const response = await fetch(`${API_BASE_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(userData),
+        body: JSON.stringify({ email, password }),
       });
 
       if (response.ok) {
-        const data = await response.json();
-        localStorage.setItem("token", data.token);
-        //navigate("/SwipePage"); //sprawdzic czy jest ok
-        setMessage("Logowanie powiodło się!");
         await router.replace("/swipePage");
       } else {
-        const errorData = await response.json();
-        setMessage(
-          `Błąd: ${errorData.message || "Wystąpił problem podczas logowania"}`
-        );
+        if(response.status === 401 || response.status === 409) {
+          setErrors({
+            global: "Złe dane logowania.",
+          });
+        }
       }
     } catch (error) {
-      setMessage("Wystąpił problem z połączeniem. Spróbuj ponownie później.");
-      console.error("Error:", error);
+      setErrors({
+        global: "Wystąpił problem z połączeniem. Spróbuj ponownie później.",
+      });
     }
   };
 
   return (
-    <div className={styles.registerFormContainer}>
-      <div className={styles.headerContainer}>
-        <h1 className={styles.title}>PETFINITY</h1>
+      <div className={styles.registerFormContainer}>
+        <div className={styles.headerContainer}>
+          <h1 className={styles.title}>PETFINITY</h1>
+        </div>
+
+        <form onSubmit={handleSubmit} className={styles.registerform} noValidate>
+          <h1 className={styles.headerRegister}>Zaloguj się!</h1>
+
+          <div className={styles.userInfoInput}>
+            <input
+                className={styles.userInput}
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                autoComplete="off"
+            />
+            {errors.email && (
+                <p className={styles.errorMessage}>{errors.email}</p>
+            )}
+          </div>
+          <div className={styles.userInfoInput}>
+            <input
+                className={styles.userInput}
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Hasło"
+                autoComplete="new-password"
+            />
+            {errors.password && <p className={styles.errorMessage}>{errors.password}</p>}
+          </div>
+          <button className={styles.registerButton} type="submit">
+            Zaloguj się
+          </button>
+          {errors.global && <p className={styles.errorMessage}>{errors.global}</p>}
+        </form>
       </div>
-      <form onSubmit={handleSubmit} className={styles.registerform}>
-        <h1 className={styles.headerRegister}>Zaloguj się!</h1>
-        <div className={styles.userInfoInput}>
-          <input
-            className={styles.userInput}
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            placeholder="Email"
-            autoComplete="off"
-          />
-        </div>
-        <div className={styles.userInfoInput}>
-          <input
-            className={styles.userInput}
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            placeholder="Hasło"
-            autoComplete="new-password"
-          />
-        </div>
-        <button className={styles.registerButton} type="submit">
-          Zaloguj się
-        </button>
-        {message && <p>{message}</p>}
-      </form>
-    </div>
   );
 };
 
