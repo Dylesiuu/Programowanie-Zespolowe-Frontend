831e7d826687ce1c9b48d4b65513447a51370daf
uwroblewska <wroblewska.ula@wp.pl>
2025-03-22 15:06:27 +0100

    Small fixes in the project

--- pages/registerPage.jsx
+++ pages/registerPage.jsx
@@ -1,16 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import RegisterForm from "../src/register/components/RegisterForm";
 import styles from "../src/styles/registerPage.module.css";
-import "normalize.css/normalize.css";
 
 const RegisterPage = () => {
-  useEffect(() => {
-    document.body.style.margin = "0";
-
-    return () => {
-      document.body.style.margin = "";
-    };
-  }, []);
   return (
     <div className={styles.registerPageContainer}>
       <RegisterForm />
