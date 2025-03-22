b4ace048031280fdaf77d2bb03da2e37999d01ac
uwroblewska <wroblewska.ula@wp.pl>
2025-03-22 15:06:27 +0100

    Small fixes in the project

--- pages/loginPage.jsx
+++ pages/loginPage.jsx
@@ -1,17 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import LoginForm from "../src/login/components/LoginForm";
 import styles from "../src/styles/registerPage.module.css";
-import "normalize.css/normalize.css";
 
 const LoginPage = () => {
-  useEffect(() => {
-    document.body.style.margin = "0";
-
-    return () => {
-      document.body.style.margin = "";
-    };
-  }, []);
-
   return (
     <div className={styles.registerPageContainer}>
       <LoginForm />
