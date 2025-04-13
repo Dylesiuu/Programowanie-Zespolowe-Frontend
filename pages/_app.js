c8c2b067dd973259488ff8a28ed1dd593912c401
NO_NAME <dylewskidom1@gmail.com>
2025-04-13 13:49:38 +0200

    Fix navbar import in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Menu from '../src/menu/components/menu';
 import '../src/styles/global.css';
 import 'normalize.css/normalize.css';
-import Navbar from '../src/navbar/components/navbar';
+import Navbar from '../src/navbar/components/Navbar';
 
 function MyApp({ Component, pageProps }) {
   return (
