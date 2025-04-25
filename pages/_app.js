10231e36feb30882533ed820f384a4e594543881
NO_NAME <dylewskidom1@gmail.com>
2025-04-25 19:32:47 +0200

    Add navbar conditional rendering options

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,22 @@ import React from 'react';
 import '../src/styles/global.css';
 import 'normalize.css/normalize.css';
 import Navbar from '../src/navbar/components/Navbar';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  const hideNavbarRoutes = [
+    '/',
+    '/userCreatorPage',
+    '/loginPage',
+    '/registerPage',
+  ];
+
   return (
     <div className="page">
-      <Navbar /> {}
+      {/* Conditionally render the Navbar */}
+      {!hideNavbarRoutes.includes(router.pathname) && <Navbar />}
       <Component {...pageProps} /> {}
     </div>
   );
