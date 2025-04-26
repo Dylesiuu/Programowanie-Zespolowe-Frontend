515b2f1179fd346e4dc4d1c85e265b94a46dd6af
NO_NAME <dylewskidom1@gmail.com>
2025-04-26 22:50:25 +0200

    Add user context to other pages

--- pages/registerPage.jsx
+++ pages/registerPage.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import RegisterForm from '../src/register/components/RegisterForm';
 import styles from '../src/styles/registerPage.module.css';
+import { UserContext } from '@/context/userContext';
+import { useRouter } from 'next/router';
 
 const RegisterPage = () => {
+  const userContext = useContext(UserContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!userContext.isLoggedIn()) {
+      return;
+    }
+
+    router.replace('/swipePage');
+  }, [router, userContext]);
+
   return (
     <div className={styles.registerPageContainer}>
       <RegisterForm />
--- pages/shelterProfilePage.jsx
+++ pages/shelterProfilePage.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import ShelterProfile from '@/shelterProfile/components/shelterProfile';
+import { UserContext } from '@/context/userContext';
+import { useRouter } from 'next/router';
 
 const ShelterProfilePage = () => {
+  const userContext = useContext(UserContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (userContext.isLoggedIn()) {
+      return;
+    }
+
+    router.replace('/');
+  }, [router, userContext]);
+
   return (
     <div className="m-0 p-0 h-[100vh] w-[100vw] bg-[url('/Union.svg')] bg-repeat bg-[length:150rem_100rem] bg-[#FFF0E9]">
       <ShelterProfile />
