e71571275575e1a0946564f4dcd0ff0a8fd6e66c
NO_NAME <dylewskidom1@gmail.com>
2025-04-26 20:34:50 +0200

    Add login check in loginForm

--- pages/loginPage.jsx
+++ pages/loginPage.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+'use client';
+import { useContext, useEffect } from 'react';
 import LoginForm from '../src/login/components/LoginForm';
 import styles from '../src/styles/registerPage.module.css';
+import { UserContext } from '@/context/userContext';
+import { useRouter } from 'next/navigation';
 
 const LoginPage = () => {
+  const userContext = useContext(UserContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!userContext.isLoggedIn()) {
+      return;
+    }
+
+    router.replace('/swipePage');
+  }, [router, userContext]);
+
   return (
     <div className={styles.registerPageContainer}>
       <LoginForm />
