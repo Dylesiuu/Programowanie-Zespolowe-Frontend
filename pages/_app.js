8af10e5436d49b5ee15b9249b33c33e2ebfc33c2
NO_NAME <dylewskidom1@gmail.com>
2025-04-26 20:34:50 +0200

    Add login check in loginForm

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../src/styles/global.css';
 import 'normalize.css/normalize.css';
 import Navbar from '../src/navbar/components/navbar';
 import { useRouter } from 'next/router';
+import { UserProvider } from '../src/context/userContext';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -15,11 +16,13 @@ function MyApp({ Component, pageProps }) {
   ];
 
   return (
-    <div className="page">
-      {/* Conditionally render the Navbar */}
-      {!hideNavbarRoutes.includes(router.pathname) && <Navbar />}
-      <Component {...pageProps} /> {}
-    </div>
+    <UserProvider>
+      <div className="page">
+        {/* Conditionally render the Navbar */}
+        {!hideNavbarRoutes.includes(router.pathname) && <Navbar />}
+        <Component {...pageProps} />
+      </div>
+    </UserProvider>
   );
 }
 
