7fd5eb8a7a177ccf3061d0f0d7ae6e18badcb8aa
NO_NAME <dylewskidom1@gmail.com>
2025-05-03 16:40:14 +0200

    Improve user logout

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -11,17 +11,36 @@ export const UserContext = createContext(initialState);
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
   const isLoggedIn = () => {
     //comment to go to normal mode
-
-    if (process.env.NODE_ENV === 'development') {
-      return true; // Always return true in development mode
-    }
+    // if (process.env.NODE_ENV === 'development') {
+    //   return true; // Always return true in development mode
+    // }
     return Boolean(user && user.token); // Normal behavior in production
   };
 
-  const logout = () => {
+  const logout = async () => {
+    if (user && user.token) {
+      try {
+        const response = await fetch(`${API_BASE_URL}/auth/logout`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+          console.warn(data.message);
+        } else {
+          console.error('Logout request failed: ', data.message);
+        }
+      } catch (error) {
+        console.error('Error during logout request:', error);
+      }
+    }
     setUser(null); // Clear user state
     localStorage.removeItem('user'); // Remove user from localStorage
     console.warn('User logged out and removed from localStorage.');
