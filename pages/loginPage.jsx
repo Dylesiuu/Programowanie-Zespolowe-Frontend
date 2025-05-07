0af12494cf900468b0a750e1eef369d3d1152ebc
NO_NAME <dylewskidom1@gmail.com>
2025-05-07 22:23:08 +0200

    Add information about the logged in user

--- pages/loginPage.jsx
+++ pages/loginPage.jsx
@@ -1,24 +1,30 @@
-'use client';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import LoginForm from '../src/login/components/LoginForm';
 import { UserContext } from '@/context/userContext';
 import { useRouter } from 'next/navigation';
+import LoggedUser from '../src/login/components/loggedUser';
 
 const LoginPage = () => {
   const userContext = useContext(UserContext);
   const router = useRouter();
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   useEffect(() => {
     if (!userContext.isLoggedIn()) {
       return;
     }
+    setIsLoggedIn(true);
 
-    router.replace('/swipePage');
+    const timeout = setTimeout(() => {
+      router.replace('/swipePage');
+    }, 1000); // Wait for 1 seconds (1000 milliseconds)
+
+    return () => clearTimeout(timeout);
   }, [router, userContext]);
 
   return (
     <div className="m-0 p-0 h-[100vh] w-[100vw] bg-[url('/Union.svg')] bg-repeat bg-[length:150rem_100rem] bg-[#FFF0E9]">
-      <LoginForm />
+      {isLoggedIn ? <LoggedUser /> : <LoginForm />}
     </div>
   );
 };
