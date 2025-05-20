c78d7c1cfac19379ebbb2d55b3e282971539e2f6
NO_NAME <dylewskidom1@gmail.com>
2025-05-20 16:35:18 +0200

    Add the ability to edit a given shelter's data in shelterCreator

--- src/navbar/components/navbar.jsx
+++ src/navbar/components/navbar.jsx
@@ -168,7 +168,7 @@ const Navbar = () => {
               </li>
               <li
                 className="px-4 py-2 hover:bg-[#AA673C] cursor-pointer"
-                onClick={() => router.push(`/shelterCreator`)}
+                onClick={() => router.push(`/shelterCreator?shelterId=null`)}
               >
                 Stwórz Schronisko
               </li>
